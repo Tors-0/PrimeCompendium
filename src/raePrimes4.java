827359cd84dc8228aed4e35c8281107e7761deb0
rae <raej0000@gmail.com>
2023-10-29 22:18:09 -0700

    comments for rae's algorithm

--- src/raePrimes4.java
+++ src/raePrimes4.java
@@ -4,19 +4,44 @@ import java.util.Scanner;
 
 public class raePrimes4 {
     static Scanner scanny = new Scanner(System.in);
-    static void makeComposite(int[] primeBools, int x) {
-        primeBools[x/64] |= (1 << ((x >> 1) & 31));
+
+    /**
+     * We use n/64 to get the index of the int x is within. Then use bitwise operators to divide x by 2, take the mod of
+     * x and 32, which gives us the index within the int at x/64. Then place a 1 at that index
+     * @param primeBooleans our "boolean" array of integers
+     * @param x the index in our "boolean" array
+     */
+    static void makeComposite(int[] primeBooleans, int x) {
+        primeBooleans[x/64] |= (1 << ((x >> 1) & 31));
     }
-    public static int isComposite(int[] primeBools, int x) {
-        return (primeBools[x/64] & (1 << ((x >> 1) & 31)));
+    /**
+     * We use n/64 to get the index of the int x is within. Then use bitwise operators to divide x by 2, take the mod of
+     * x and 32, which gives us the index within the int at x/64. Then bitwise AND the binary at that bit with 1 and
+     * compare that output with 1 (the binary representation of {@link Boolean#TRUE})
+     * @param primeBools our "boolean" array of integers
+     * @param x the index in the "boolean" array
+     * @return true is x is composite, false if prime
+     */
+    public static boolean isComposite(int[] primeBools, int x) {
+        return (primeBools[x/64] & (1 << ((x >> 1) & 31))) == 1;
     }
+
+    /**
+     * Revamped algorithm using bitwise functionality to optimize processing time and memory density
+     * @param n An int <i>n</i>
+     * @return The <i>n</i>th prime number
+     */
     public static long primeSieve(int n) {
         int estimatedLimit = (int)((Math.log(n) + Math.log(Math.log(n)) - 1 + ( (Math.log(Math.log(n)) - 2) / Math.log(n) )
                 - (( Math.pow(Math.log(Math.log(n)),2) - 6 * Math.log(Math.log(n)) - 11 )/( 2 * Math.pow(Math.log(n),2) ))) * n);
+        /*
+         * We're using each individual bit of an int (32 bit) array as a boolean, and only storing odd numbers, so we can
+         * cut the array size down to n/64. +1 for OBOE protection
+         */
         int[] isPrimeBools = new int[estimatedLimit/64 + 1];
         Arrays.fill(isPrimeBools, 0000);
         for (int i = 3; i * i <= estimatedLimit; i+=2) {
-            if (isComposite(isPrimeBools, i) == 0) {
+            if (!isComposite(isPrimeBools, i)) {
                 for (int j = i*i, k = i << 1; j < estimatedLimit; j += k) {
                     makeComposite(isPrimeBools, j);
                 }
@@ -24,7 +49,7 @@ public class raePrimes4 {
         }
         int finalPrime = 1;
         for (int foundPrimes = 1; foundPrimes <= n; finalPrime += 2) {
-            if (isComposite(isPrimeBools, finalPrime) == 0) {
+            if (!isComposite(isPrimeBools, finalPrime)) {
                 foundPrimes++;
             }
         }
